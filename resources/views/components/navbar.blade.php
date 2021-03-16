9ade0e5d5eac442f5c18b8f17e99754842eb9bb9
d3m37r4 <dmitrij.isakoff2012@yandex.ru>
2021-03-16 20:00:05 +0700

    Fix

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -38,9 +38,7 @@
                                 <li><a class="dropdown-item" href="#">Another action</a></li>
                                 <li><hr class="dropdown-divider"></li>
                                 <li>
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                document.getElementById('logout-form').submit();">
+                                    <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
                                         {{ ('Выход') }}
                                     </a>
                                     <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
