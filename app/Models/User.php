e8f766bb51144764f4791d02a864aee5a51c645f
d3m37r4 <dmitrij.isakoff2012@yandex.ru>
2021-03-24 22:13:04 +0700

    Ide helper changes

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,12 +2,17 @@
 
 namespace App\Models;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
+//use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Spatie\Permission\Traits\HasRoles;
 use Illuminate\Notifications\Notifiable;
 
+/**
+ * @method static paginate(mixed $env)
+ * @method static find(int $id)
+ * @method static findOrFail(int $id)
+ */
 class User extends Authenticatable
 {
     use HasFactory, Notifiable, HasRoles;
