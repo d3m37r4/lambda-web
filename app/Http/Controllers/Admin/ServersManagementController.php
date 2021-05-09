62d37a6799e71c2e938c33a271d8d9cd83a367e6
d3m37r4 <dmitrij.isakoff2012@yandex.ru>
2021-05-09 21:31:16 +0700

    Added a form for adding servers

--- app/Http/Controllers/Admin/ServersManagementController.php
+++ app/Http/Controllers/Admin/ServersManagementController.php
@@ -4,11 +4,14 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Models\Server;
+use Exception;
 use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Contracts\View\Factory;
 use Illuminate\Contracts\View\View;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Validator;
 
 class ServersManagementController extends Controller {
     /**
@@ -25,23 +28,44 @@ class ServersManagementController extends Controller {
     /**
      * Show the form for creating a new server.
      *
-     * @return Response
+     * @return Application|Factory|View|Response
      */
-//    public function create()
-//    {
-//
-//    }
+    public function create() {
+        return view('admin.servers.create');
+    }
 
     /**
      * Store a newly created server in storage.
      *
-     * @param  Request  $request
-     * @return Response
+     * @param Request $request
+     * @return RedirectResponse
+     * @throws Exception
      */
-//    public function store(Request $request)
-//    {
-//
-//    }
+    public function store(Request $request): RedirectResponse {
+        $rules = [
+            'name' => ['required', 'string', 'max:255', 'unique:servers'],
+            'ip' => ['required', 'ip'],
+            'port' => ['required', 'integer', 'between:1,65535'],
+            'rcon' => ['string', 'max:128'],
+        ];
+        $validator = Validator::make($request->all(), $rules);
+
+        if ($validator->fails()) {
+            return back()->withErrors($validator)->withInput();
+        }
+
+        $server = Server::create([
+            'name' => strip_tags($request->input('name')),
+            'ip' => $request->input('ip'),
+            'port' => $request->input('port'),
+            'token' => (new Server)->generateSecurityToken(),
+        ]);
+
+        return redirect()
+            ->route('admin.servers.index')
+            ->with('status', 'success')
+            ->with('message', "Сервер {$server->name} успешно добавлен!");
+    }
 
     /**
      * Display the specified server.
