c087f297c40cd7065c9fd1806d9cf42a48cbddcc
d3m37r4 <dmitrij.isakoff2012@yandex.ru>
2021-05-09 15:27:25 +0700

    Added method in phpdoc

--- app/Models/Server.php
+++ app/Models/Server.php
@@ -8,6 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 /**
  * @method static create(array $array)
  * @method static where(string $string, string $token)
+ * @method static paginate(mixed $env)
  */
 class Server extends Model {
     /**
