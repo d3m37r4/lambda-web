ccdbaf68fd607af9948d40e5c0db980906876f61
d3m37r4 <dmitrij.isakoff2012@yandex.ru>
2021-05-09 15:14:56 +0700

    Extension of model 'Server'

--- app/Models/Server.php
+++ app/Models/Server.php
@@ -2,10 +2,52 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Exception;
 use Illuminate\Database\Eloquent\Model;
 
-class Server extends Model
-{
-    use HasFactory;
+/**
+ * @method static create(array $array)
+ * @method static where(string $string, string $token)
+ */
+class Server extends Model {
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'servers';
+
+    /**
+     * @var string
+     */
+    protected $primaryKey = 'id';
+
+    /**
+     * @var array
+     */
+    protected $fillable = ['name', 'ip', 'port', 'rcon', 'token'];
+
+    /**
+     * @var array
+     */
+    protected $dates = ['created_at', 'updated_at'];
+
+    /**
+     * @var array
+     */
+    protected $hidden = ['rcon', 'token', 'created_at', 'updated_at'];
+
+    /**
+     * @return string
+     * @throws Exception
+     */
+    public function generateSecurityToken(): string {
+        $tries = 0;
+
+        do {
+            $token = bin2hex(random_bytes(32));
+        } while (++$tries < 3 && Server::where('token', $token)->exists());
+
+        return $token;
+    }
 }
