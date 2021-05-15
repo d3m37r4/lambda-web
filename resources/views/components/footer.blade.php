07f673cc595c9879afc4cdc8efab3330be196c56
d3m37r4 <dmitrij.isakoff2012@yandex.ru>
2021-05-15 16:29:07 +0700

    Removed outline of button scrollUp.

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -3,6 +3,6 @@
         <span class="text-muted"><a class="link-dark" href="/">{{ config('app.name') }}</a> © 2021</span>
     </div>
     <div>
-        <a class="btn btn-outline-secondary btn-sm rounded-circle" href="#" title="Вверх"><i class="bi bi-chevron-up"></i></a>
+        <a class="btn btn-sm rounded-circle" href="#" title="Вверх"><i class="bi bi-chevron-up"></i></a>
     </div>
 </div>
