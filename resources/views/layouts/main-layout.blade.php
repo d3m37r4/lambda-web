949605af6cf6e97fbbeb131ec7fac4d1dfad4d34
d3m37r4 <dmitrij.isakoff2012@yandex.ru>
2021-05-29 22:57:12 +0700

    Added font awesome icons

--- resources/views/layouts/main-layout.blade.php
+++ resources/views/layouts/main-layout.blade.php
@@ -21,6 +21,11 @@
     <!-- Bootstrap Icons -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.0/font/bootstrap-icons.css">
 
+    <!-- Font Awesome Icons -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"
+          integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w=="
+          crossorigin="anonymous" referrerpolicy="no-referrer"/>
+
     <!-- Custom CSS -->
     <link rel="stylesheet" href="{{ asset('css/custom.style.css') }}">
 </head>
