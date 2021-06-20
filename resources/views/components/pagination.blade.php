cef2e2cd7038ab1b0c2a8f0470b6e9091b57b159
d3m37r4 <dmitrij.isakoff2012@yandex.ru>
2021-06-20 23:02:46 +0700

    Added use of MDBootstrap (css+js). Removed bootstrap icons. Edits in templates. Pagination, navbar, footer changed.

--- resources/views/components/pagination.blade.php
+++ resources/views/components/pagination.blade.php
@@ -3,12 +3,17 @@
         <ul class="pagination">
             {{-- Previous Page Link --}}
             @if ($paginator->onFirstPage())
-                <li class="page-item disabled" aria-disabled="true" aria-label="@lang('pagination.previous')">
-                    <span class="page-link" aria-hidden="true">&lsaquo;</span>
+                <li class="page-item disabled pe-1" aria-disabled="true" aria-label="@lang('pagination.previous')">
+                    <span class="page-link" aria-hidden="true">
+                        {{ ('Назад') }}
+                    </span>
                 </li>
             @else
                 <li class="page-item">
-                    <a class="page-link" href="{{ $paginator->previousPageUrl() }}" rel="prev" aria-label="@lang('pagination.previous')">&lsaquo;</a>
+                    <a class="page-link pe-1" href="{{ $paginator->previousPageUrl() }}" rel="prev"
+                       aria-label="@lang('pagination.previous')">
+                        {{ ('Назад') }}
+                    </a>
                 </li>
             @endif
 
@@ -16,16 +21,28 @@
             @foreach ($elements as $element)
                 {{-- "Three Dots" Separator --}}
                 @if (is_string($element))
-                    <li class="page-item disabled" aria-disabled="true"><span class="page-link">{{ $element }}</span></li>
+                    <li class="page-item disabled px-1" aria-disabled="true">
+                        <span class="page-link">
+                            {{ $element }}
+                        </span>
+                    </li>
                 @endif
 
                 {{-- Array Of Links --}}
                 @if (is_array($element))
                     @foreach ($element as $page => $url)
                         @if ($page == $paginator->currentPage())
-                            <li class="page-item active" aria-current="page"><span class="page-link">{{ $page }}</span></li>
+                            <li class="page-item active px-1" aria-current="page">
+                                <span class="page-link">
+                                    {{ $page }}
+                                </span>
+                            </li>
                         @else
-                            <li class="page-item"><a class="page-link" href="{{ $url }}">{{ $page }}</a></li>
+                            <li class="page-item px-1">
+                                <a class="page-link" href="{{ $url }}">
+                                    {{ $page }}
+                                </a>
+                            </li>
                         @endif
                     @endforeach
                 @endif
@@ -33,12 +50,17 @@
 
             {{-- Next Page Link --}}
             @if ($paginator->hasMorePages())
-                <li class="page-item">
-                    <a class="page-link" href="{{ $paginator->nextPageUrl() }}" rel="next" aria-label="@lang('pagination.next')">&rsaquo;</a>
+                <li class="page-item ps-1">
+                    <a class="page-link" href="{{ $paginator->nextPageUrl() }}" rel="next"
+                       aria-label="@lang('pagination.next')">
+                        {{ ('Далее') }}
+                    </a>
                 </li>
             @else
-                <li class="page-item disabled" aria-disabled="true" aria-label="@lang('pagination.next')">
-                    <span class="page-link" aria-hidden="true">&rsaquo;</span>
+                <li class="page-item disabled ps-1" aria-disabled="true" aria-label="@lang('pagination.next')">
+                    <span class="page-link" aria-hidden="true">
+                        {{ ('Далее') }}
+                    </span>
                 </li>
             @endif
         </ul>
--- resources/views/layouts/admin-layout.blade.php
+++ resources/views/layouts/admin-layout.blade.php
@@ -4,12 +4,11 @@
 
 @section('content')
 {{--    @include('admin.components.breadcrubms')--}}
-
     <div class="row justify-content-center">
-        <div class="col-md-3 mb-3">
+        <div class="col-md-3 mb-3 pt-3">
             @include('admin.components.sidebar')
         </div>
-        <div class="col-md-9 mb-3">
+        <div class="col-md-9 mb-3 pt-3">
             @yield('admin.content')
         </div>
     </div>
