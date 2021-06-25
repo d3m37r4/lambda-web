e7d714724679d2484d4941f5652ac3a4021a9c7c
d3m37r4 <dmitrij.isakoff2012@yandex.ru>
2021-06-25 21:41:02 +0700

    Added an outline of the representation of displaying complete information about the user in admin panel.

--- resources/views/admin/users/show.blade.php
+++ resources/views/admin/users/show.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.admin-layout')
 
-@section('title', 'Профиль пользователя')
+@section('title', "Профиль пользователя $user->name")
 
 @section('admin.content')
     <div class="card shadow-2 border">
@@ -8,7 +8,7 @@
             <div class="d-sm-flex justify-content-between">
                 <div class="me-auto align-self-center">
                     <h5 class="card-title m-0">
-                        <i class="fas fa-user-edit"></i>
+                        <i class="fas fa-user"></i>
                         {{ ('Профиль пользователя: ') }} {{ $user->name }}
                     </h5>
                 </div>
@@ -18,56 +18,103 @@
             </div>
         </div>
         <div class="card-body">
-            Имя пользователя: {{ $user->name }} <br />
-            UserID: {{ $user->id }} <br />
-            Роль:  <span class="badge bg-primary">{{ $user->getRoleNames()->first() }}</span> <br />
-            <button class="btn btn-primary" type="button" data-mdb-toggle="collapse" data-mdb-target="#showPermissions" aria-expanded="false" aria-controls="showPermissions">
-                Показать разрешения пользователя
-            </button>
-
-            <div class="collapse" id="showPermissions">
-                @foreach($permissions as $permission)
-                    <span class="badge bg-primary">{{ $permission->name }} </span><br />
-                @endforeach
+            <div class="row">
+                <div class="col-md-3">
+                    <div class="d-flex flex-column align-items-center text-center border-bottom pb-3">
+                        <i class="fas fa-user fa-10x"></i>
+                        <div class="mt-3">
+                            <p class="text-secondary mb-1">
+                                <span class="badge bg-primary">{{ $user->getRoleNames()->first() }}</span>
+                            </p>
+                            <p class="text-muted font-size-sm">Bay Area, San Francisco, CA</p>
+                        </div>
+                    </div>
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item d-flex justify-content-between align-items-center flex-wrap">
+                            <span style="color:#202020;">
+                                <i class="fab fa-steam fa-lg"></i>
+                            </span>
+                            <span class="text-secondary">bootdey</span>
+                        </li>
+                        <li class="list-group-item d-flex justify-content-between align-items-center flex-wrap">
+                            <span style="color:#2787F5;">
+                                <i class="fab fa-vk fa-lg"></i>
+                            </span>
+                            <span class="text-secondary">
+                                bootdey
+                            </span>
+                        </li>
+                        <li class="list-group-item d-flex justify-content-between align-items-center flex-wrap">
+                            <span style="color:#0088cc;">
+                                <i class="fab fa-telegram fa-lg"></i>
+                            </span>
+                            <span class="text-secondary">bootdey</span>
+                        </li>
+                    </ul>
+                </div>
+                <div class="col-md-9">
+                    <div class="pb-3">
+                        <ul class="nav nav-tabs nav-justified mb-3" id="ex1" role="tablist">
+                            <li class="nav-item" role="presentation">
+                                <a class="nav-link active" href="#ex1-tabs-1" id="ex1-tab-1" data-mdb-toggle="tab"
+                                   role="tab" aria-controls="ex1-tabs-1" aria-selected="true">
+                                    {{ ('Общая информация') }}
+                                </a>
+                            </li>
+                            <li class="nav-item" role="presentation">
+                                <a class="nav-link" href="#ex1-tabs-2" id="ex1-tab-2" data-mdb-toggle="tab"
+                                    role="tab" aria-controls="ex1-tabs-2" aria-selected="false">
+                                    {{ ('Доступные разрешения') }}
+                                </a>
+                            </li>
+                            <li class="nav-item" role="presentation">
+                                <a class="nav-link" href="#ex1-tabs-3" id="ex1-tab-3" data-mdb-toggle="tab"
+                                    role="tab" aria-controls="ex1-tabs-3" aria-selected="false">
+                                    {{ ('Игровые аккаунты') }}
+                                </a>
+                            </li>
+                        </ul>
+                        <div class="tab-content" id="ex1-content">
+                            <div class="tab-pane fade show active" id="ex1-tabs-1" role="tabpanel"
+                                 aria-labelledby="ex1-tab-1">
+                                Имя: {{ $user->name }} <br />
+                                Фамилия: {{ $user->name }} <br />
+                                UserID: {{ $user->id }} <br />
+                                Эл. почта: <a class="link-primary"
+                                   data-mdb-toggle="tooltip"
+                                   title="Email {{ $user->email }}"
+                                   href="mailto:{{ $user->email }}">
+                                    {{ $user->email }}
+                                </a> <br />
+                                Зарегистрирован: {{ $user->created_at }} <br />
+                                Последнее обновление профиля: {{ $user->updated_at }} <br />
+                            </div>
+                            <div class="tab-pane fade" id="ex1-tabs-2" role="tabpanel" aria-labelledby="ex1-tab-2">
+                                @foreach($permissions as $permission)
+                                    <span class="badge bg-primary">{{ $permission->name }} </span><br />
+                                @endforeach
+                            </div>
+                            <div class="tab-pane fade" id="ex1-tabs-3" role="tabpanel" aria-labelledby="ex1-tab-3">
+                                Игровые аккаунты
+                            </div>
+                        </div>
+                    </div>
+{{--                    <div class="row">--}}
+{{--                        <div>--}}
+{{--                            .div--}}
+{{--                        </div>--}}
+{{--                        <div>--}}
+{{--                            .div--}}
+{{--                        </div>--}}
+{{--                        <div class="col-md-6">--}}
+{{--                            .col-md-6--}}
+{{--                        </div>--}}
+{{--                        <div class="col-md-6">--}}
+{{--                            .col-md-6--}}
+{{--                        </div>--}}
+{{--                    </div>--}}
+                </div>
             </div>
-            <br />
-            Эл. почта: <a class="link-primary"
-               data-mdb-toggle="tooltip"
-               title="Email {{ $user->email }}"
-               href="mailto:{{ $user->email }}">
-                {{ $user->email }}
-            </a> <br />
-            Зарегистрирован: {{ $user->created_at }} <br />
-            Последнее обновление профиля: {{ $user->updated_at }} <br />
-            <!-- Facebook -->
-            <a class="btn btn-primary" style="background-color: #3b5998;" href="#!" role="button"
-            ><i class="fab fa-facebook-f"></i
-                ></a>
-
-            <!-- Twitter -->
-            <a class="btn btn-primary" style="background-color: #55acee;" href="#!" role="button"
-            ><i class="fab fa-twitter"></i
-                ></a>
-
-            <!-- Google -->
-            <a class="btn btn-primary" style="background-color: #dd4b39;" href="#!" role="button"
-            ><i class="fab fa-google"></i
-                ></a>
-
-            <!-- Instagram -->
-            <a class="btn btn-primary" style="background-color: #ac2bac;" href="#!" role="button"
-            ><i class="fab fa-instagram"></i
-                ></a>
-
-            <!-- Vkontakte -->
-            <a class="btn btn-primary" style="background-color: #4c75a3;" href="#!" role="button"
-            ><i class="fab fa-vk"></i
-                ></a>
-
-            <!-- Whatsapp -->
-            <a class="btn btn-primary" style="background-color: #25d366;" href="#!" role="button"
-            ><i class="fab fa-whatsapp"></i
-                ></a>
         </div>
     </div>
 @endsection
