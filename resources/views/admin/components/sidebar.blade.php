700a57730c4bd09300c2899524662c05687c4c4e
d3m37r4 <dmitrij.isakoff2012@yandex.ru>
2021-06-25 22:09:49 +0700

    Small changes in pagination and sidebar styles

--- resources/views/admin/components/sidebar.blade.php
+++ resources/views/admin/components/sidebar.blade.php
@@ -1,21 +1,21 @@
 <div class="list-group shadow-2">
-    <a class="list-group-item list-group-item-action {{ Request::routeIs('admin.index') ? 'active' : '' }}"
-       href="{{ route('admin.index') }}">
+    <a class="list-group-item list-group-item-action {{ Request::routeIs('admin.index') ? 'active' : '' }} ripple"
+       href="{{ route('admin.index') }}" data-mdb-ripple-color="light">
         <i class="fas fa-cogs"></i>
         {{ ('Панель управления') }}
     </a>
-    <a class="list-group-item list-group-item-action {{ Request::routeIs('admin.users.*') ? 'active' : '' }}"
-       href="{{ route('admin.users.index') }}">
+    <a class="list-group-item list-group-item-action {{ Request::routeIs('admin.users.*') ? 'active' : '' }} ripple"
+       href="{{ route('admin.users.index') }}" data-mdb-ripple-color="light">
         <i class="fas fa-users-cog"></i>
         {{ ('Управление пользователями') }}
     </a>
-    <a class="list-group-item list-group-item-action {{ Request::routeIs('admin.roles.*') ? 'active' : '' }}"
-       href="{{ route('admin.roles.index') }}">
+    <a class="list-group-item list-group-item-action {{ Request::routeIs('admin.roles.*') ? 'active' : '' }} ripple"
+       href="{{ route('admin.roles.index') }}" data-mdb-ripple-color="light">
         <i class="fas fa-user-shield"></i>
         {{ ('Управление ролями') }}
     </a>
-    <a class="list-group-item list-group-item-action {{ Request::routeIs('admin.servers.*') ? 'active' : '' }}"
-       href="{{ route('admin.servers.index') }}">
+    <a class="list-group-item list-group-item-action {{ Request::routeIs('admin.servers.*') ? 'active' : '' }} ripple"
+       href="{{ route('admin.servers.index') }}" data-mdb-ripple-color="light">
         <i class="fas fa-server"></i>
         {{ ('Управление серверами') }}
     </a>
