9c8a565ab9a8052a340813a1dad3080866e1ae41
d3m37r4 <dmitrij.isakoff2012@yandex.ru>
2021-07-24 11:20:36 +0700

    Added property of map name field

--- database/migrations/2021_07_24_034644_create_maps_table.php
+++ database/migrations/2021_07_24_034644_create_maps_table.php
@@ -15,7 +15,7 @@ class CreateMapsTable extends Migration
     {
         Schema::create('maps', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('name')->unique();
             $table->timestamps();
         });
     }
