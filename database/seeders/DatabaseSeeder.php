ed0069fc8cd74ccb771559ce9776f197c0b9525f
d3m37r4 <dmitrij.isakoff2012@yandex.ru>
2021-07-24 11:14:53 +0700

    Map model, seeding, migration and relation to servers have been created

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,5 +18,6 @@ class DatabaseSeeder extends Seeder
         $this->call(RoleSeeder::class);
         $this->call(UserSeeder::class);
         $this->call(ServerSeeder::class);
+        $this->call(MapSeeder::class);
     }
 }
