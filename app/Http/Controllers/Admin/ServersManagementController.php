d17ca0015ec8df3057a62c20ee6fcaaf7cd39aa7
d3m37r4 <dmitrij.isakoff2012@yandex.ru>
2021-09-11 16:46:10 +0700

    Validation for Update method is placed in a separate UpdateServerRequest class. Removed unused facades in controller. Minor changes in the description of methods and return values.

--- app/Http/Controllers/Admin/ServersManagementController.php
+++ app/Http/Controllers/Admin/ServersManagementController.php
@@ -6,23 +6,18 @@ use Carbon\Carbon;
 use App\Models\Server;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\StoreServerRequest;
-use Illuminate\Contracts\Foundation\Application;
-use Illuminate\Contracts\View\Factory;
+use App\Http\Requests\UpdateServerRequest;
 use Illuminate\Contracts\View\View;
-use Illuminate\Http\Response;
 use Illuminate\Http\RedirectResponse;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\URL;
-use Illuminate\Support\Facades\Validator;
 
 class ServersManagementController extends Controller {
     /**
      * Display a listing of the servers.
      *
-     * @return Application|Factory|View|Response
+     * @return View
      */
-    public function index() {
+    public function index(): View {
         $servers = Server::paginate(env('PAGINATION_SIZE'));
 
         return view('admin.servers.index', compact('servers'));
@@ -31,9 +26,9 @@ class ServersManagementController extends Controller {
     /**
      * Show the form for creating a new server.
      *
-     * @return Application|Factory|View|Response
+     * @return View
      */
-    public function create() {
+    public function create(): View {
         $createdTime = Carbon::now()->format('d.m.Y - H:i:s');
         $redirect = $this->getPreviousUrl(action([ServersManagementController::class, 'index']));
 
@@ -59,9 +54,9 @@ class ServersManagementController extends Controller {
      * Display the specified server.
      *
      * @param Server $server
-     * @return Application|Factory|View|Response
+     * @return View
      */
-    public function show(Server $server) {
+    public function show(Server $server): View {
         $redirect = $this->getPreviousUrl(action([ServersManagementController::class, 'index']));
 
         return view('admin.servers.show', compact('server', 'redirect'));
@@ -71,69 +66,28 @@ class ServersManagementController extends Controller {
      * Show the form for editing the specified server.
      *
      * @param Server $server
-     * @return Application|Factory|View|Response
+     * @return View
      */
-    public function edit(Server $server) {
+    public function edit(Server $server): View {
         $redirect = $this->getPreviousUrl(action([ServersManagementController::class, 'index']));
+
         return view('admin.servers.edit', compact('server','redirect'));
     }
 
     /**
      * Update the specified server in storage.
      *
-     * @param  Request  $request
+     * @param UpdateServerRequest $request
      * @param Server $server
      * @return RedirectResponse
      */
-    public function update(Request $request, Server $server): RedirectResponse {
-        $nameCheck = !empty($request->input('name')) && ($request->input('name') != $server->name);
-        $ipCheck = !empty($request->input('ip')) && ($request->input('ip') != $server->ip);
-        $portCheck = !empty($request->input('port')) && ($request->input('port') != $server->port);
+    public function update(UpdateServerRequest $request, Server $server): RedirectResponse {
+        $server->update($request->validated());
 
-        if ($nameCheck) {
-            $rules['name'] = ['required', 'string', 'max:255', 'unique:servers'];
-        }
-
-        if ($ipCheck) {
-            $rules['ip'] = ['required', 'ip'];
-        }
-
-        if ($portCheck) {
-            $rules['port'] = ['required', 'integer', 'between:1,65535'];
-        }
-
-        if (isset($rules)) {
-            $validator = Validator::make($request->all(), $rules);
-            if ($validator->fails()) {
-                return back()->withErrors($validator)->withInput();
-            }
-        }
-
-        if ($nameCheck) {
-            $server->name = strip_tags($request->input('name'));
-        }
-
-        if ($ipCheck) {
-            $server->ip = $request->input('ip');
-        }
-
-        if ($portCheck) {
-            $server->port = $request->input('port');
-        }
-
-        $authTokenHash = Hash::make($request->input('auth_token'));
-        if ($this->checkAuthTokenUniqueByHash($authTokenHash)) {
-            $server->auth_token = $authTokenHash;
-        } else {
-            return $this->redirectBack();
-        }
-
-        $server->rcon = $request->input('rcon');
-        $server->save();
-
-        return back()
-            ->with('status', 'success')
-            ->with('message', "Информация о сервере {$server->name} успешно обновлена!");
+        return back()->with([
+            'status' => 'success',
+            'message' => "Информация о сервере {$server->name} успешно обновлена!"
+        ]);
     }
 
     /**
@@ -144,31 +98,11 @@ class ServersManagementController extends Controller {
      */
     public function destroy(Server $server): RedirectResponse {
         $server->delete();
-        return back()
-            ->with('status', 'success')
-            ->with('message', "Сервер {$server->name} удален!");
-    }
 
-    /**
-     * Checks uniqueness of token by hash.
-     * If record is not found, it returns true, otherwise false.
-     *
-     * @param $hash
-     * @return bool
-     */
-    protected function checkAuthTokenUniqueByHash($hash): bool {
-        return (bool)Server::where('auth_token', $hash)->doesntExist();
-    }
-
-    /**
-     * Redirects to previous page and shows notification.
-     *
-     * @return RedirectResponse
-     */
-    protected function redirectBack(): RedirectResponse {
-        return back()
-            ->with('status', 'danger')
-            ->with('message', "Токен уже используется системой! Пожалуйста, сгенерируйте новый токен.");
+        return back()->with([
+            'status' => 'success',
+            'message' => "Сервер {$server->name} удален!"
+        ]);
     }
 
     /**
