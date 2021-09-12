21f817bb22f431895da3d52ff30f2308bb1e10b2
d3m37r4 <dmitrij.isakoff2012@yandex.ru>
2021-09-12 17:03:17 +0700

    Removed method that determines access to fill-in form. It is automatically retrieved from StoreRoleRequest.

--- app/Http/Requests/UpdateRoleRequest.php
+++ app/Http/Requests/UpdateRoleRequest.php
@@ -9,15 +9,6 @@ use Illuminate\Validation\Rule;
  */
 class UpdateRoleRequest extends StoreRoleRequest {
     /**
-     * Determine if the user is authorized to make this request.
-     *
-     * @return bool
-     */
-    public function authorize(): bool {
-        return true;
-    }
-
-    /**
      * Get the validation rules that apply to the request.
      *
      * @return array
