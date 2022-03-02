a143f3ceb94fa78d453874e26212d88b5e1d5fd5
d3m37r4 <dmitrij.isakoff2012@yandex.ru>
2022-03-02 13:40:47 +0700

    Updated player model class.

--- app/Models/Player.php
+++ app/Models/Player.php
@@ -3,8 +3,49 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
+/**
+ * @method static firstOrNew(array $array, array $array1)
+ */
 class Player extends Model
 {
-    //
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'user_id',
+        'server_id',
+        'name',
+        'authid',
+        'ip',
+        'auth_type',
+        'is_online'
+    ];
+
+    /**
+     * @var array
+     */
+    protected $dates = ['created_at', 'updated_at'];
+
+    /**
+     * @var array
+     */
+    protected $casts = [
+        'authid' => 'string',
+        'ip' => 'string',
+        'auth_type' => 'string',
+        'is_online' => 'boolean'
+    ];
+
+    /**
+     * Gets server associated with this player.
+     * @return BelongsTo
+     */
+    public function server(): BelongsTo
+    {
+        return $this->belongsTo(Server::class);
+    }
 }
