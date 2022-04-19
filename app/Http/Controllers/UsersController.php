bb46e79919c8220fca910417f48566e8102bbf49
d3m37r4 <dmitrij.isakoff2012@yandex.ru>
2022-04-19 23:11:09 +0700

    Minor changes in 'UsersController' controller.

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -3,8 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use Auth;
 use Illuminate\Http\Request;
 use Illuminate\Contracts\View\View;
+use Illuminate\Http\Response;
 
 class UsersController extends Controller
 {
@@ -29,26 +31,30 @@ class UsersController extends Controller
         return view('users.show', compact('user'));
     }
 
-//    /**
-//     * Show the form for editing the specified resource.
-//     *
-//     * @param  \App\Models\User  $user
-//     * @return \Illuminate\Http\Response
-//     */
-//    public function edit(User $user)
-//    {
-//        //
-//    }
+    /**
+     * Show the form for editing the specified user profile.
+     *
+     * @param  User  $user
+     * @return View
+     */
+    public function edit(User $user): View
+    {
+        if(Auth::user()->id != $user->id) {
+            abort(404);
+        }
 
-//    /**
-//     * Update the specified resource in storage.
-//     *
-//     * @param  \Illuminate\Http\Request  $request
-//     * @param  \App\Models\User  $user
-//     * @return \Illuminate\Http\Response
-//     */
-//    public function update(Request $request, User $user)
-//    {
-//        //
-//    }
+        return view('users.edit', compact('user'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param Request $request
+     * @param  User  $user
+     * @return Response
+     */
+    public function update(Request $request, User $user): Response
+    {
+        //
+    }
 }
