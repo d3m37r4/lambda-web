1f4265f971a15c9ff0a2251b0f75e219bcdfe16f
d3m37r4 <dmitrij.isakoff2012@yandex.ru>
2022-04-24 13:16:26 +0700

    Added the function of getting users with the specified country.

--- app/Models/Country.php
+++ app/Models/Country.php
@@ -3,11 +3,26 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Collection;
 
+/**
+ * @property int id
+ * @method static find(int $int)
+ */
 class Country extends Model
 {
     protected $fillable = [
         'default_name',
-        'short_code',
+        'short_code'
     ];
+
+    /**
+     * Gets users associated with this country.
+     *
+     * @return Collection
+     */
+    public function users(): Collection
+    {
+        return User::where('country_id', $this->id)->get();
+    }
 }
