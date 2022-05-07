21885b2f2ddc4b3a31533ef862cc0db138a4e610
d3m37r4 <dmitrij.isakoff2012@yandex.ru>
2022-05-07 12:50:47 +0700

    Changes in migration for the user table.

--- database/migrations/2022_04_20_131318_add_fields_to_users_table.php
+++ database/migrations/2022_04_20_131318_add_fields_to_users_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -14,16 +15,12 @@ class AddFieldsToUsersTable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
+            $table->renameColumn('name', 'login');
             $table->after('password', function ($table) {
-                $table->ipAddress('ip');
+//                $table->ipAddress('ip')->nullable();
                 $table->foreignId('country_id')->nullable()->constrained('countries');
                 $table->text('full_name')->nullable();
-                $table->enum('gender', [
-                    'male',
-                    'female',
-                    'gender x',
-                    'not specified'
-                ])->default('not specified');
+                $table->enum('gender', User::GENDERS)->default(User::GENDER_NONE);
                 $table->date('date_of_birth')->nullable();
                 $table->text('biography')->nullable();
             });
@@ -38,8 +35,9 @@ class AddFieldsToUsersTable extends Migration
     public function down()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->dropColumn('ip');
-            $table->dropColumn('country_id');
+            $table->renameColumn('login', 'name');
+//            $table->dropColumn('ip');
+            $table->dropForeign('country_id');
             $table->dropColumn('full_name');
             $table->dropColumn('gender');
             $table->dropColumn('date_of_birth');
