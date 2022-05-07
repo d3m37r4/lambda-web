5a3bccdd2f778344b638d7d0d8dc7452975b94c7
d3m37r4 <dmitrij.isakoff2012@yandex.ru>
2022-05-07 12:55:53 +0700

    Changes in routes intended for actions with users.

--- routes/web.php
+++ routes/web.php
@@ -1,33 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Auth\RegisterStep2Controller;
 use App\Http\Controllers\UsersController;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
 Route::get('/', function () {
     return view('welcome');
 });
-
-Auth::routes();
-
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::group([
     'middleware' => ['web', 'auth']
 ], function () {
-    Route::get('register-step2', [RegisterStep2Controller::class, 'showStep2Form']);
-    Route::post('register-step2', [RegisterStep2Controller::class, 'postStep2Form'])->name('register.step2');
-
-    Route::resource('users', UsersController::class)
-        ->except(['create', 'store', 'destroy']);
+    Route::resource('users', UsersController::class)->only(['show', 'edit', 'update']);
 });
