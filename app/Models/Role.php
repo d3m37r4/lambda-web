890fa55102b9c42d041457ae0ca944828697ccd0
d3m37r4 <dmitrij.isakoff2012@yandex.ru>
2022-05-07 12:48:32 +0700

    Added role constants to the 'Role' class.

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -10,5 +10,8 @@ use Spatie\Permission\Models\Role as SpatieRole;
  */
 class Role extends SpatieRole
 {
-    //
+    const ROLE_OWNER = 'owner';
+    const ROLE_ADMIN = 'admin';
+    const ROLE_MODER = 'moder';
+    const ROLE_USER = 'user';
 }
