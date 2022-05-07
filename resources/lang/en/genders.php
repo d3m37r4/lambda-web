531096a6092af32105fcd3508448c0e10335aa1d
d3m37r4 <dmitrij.isakoff2012@yandex.ru>
2022-05-07 12:51:30 +0700

    Changes in lang files.

---
+++ resources/lang/en/genders.php
@@ -0,0 +1,13 @@
+<?php
+
+use App\Models\User;
+
+return [
+    'gender' => 'Gender',
+    'list' => [
+        User::GENDER_NONE => 'Not Specified',
+        User::GENDER_MALE => 'Male',
+        User::GENDER_FEMALE => 'Female',
+        User::GENDER_X => 'Gender X'
+    ]
+];
