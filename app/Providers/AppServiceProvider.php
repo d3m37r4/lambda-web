92bbdfb370b635b1c045d595fc6ea01854fcf153
d3m37r4 <dmitrij.isakoff2012@yandex.ru>
2022-05-07 12:59:13 +0700

    Standard rules for user password validation have been added.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,9 +2,11 @@
 
 namespace App\Providers;
 
+use App\Models\User;
 use Carbon\CarbonInterval;
 use Illuminate\Pagination\Paginator;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Validation\Rules\Password;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -29,5 +31,13 @@ class AppServiceProvider extends ServiceProvider
 
         // TODO: change on CarbonInterval::setLocale(Config::get('app.locale'));
         CarbonInterval::setLocale('ru');
+
+        // Defining default password rules
+        Password::defaults(function () {
+            return User::PASSWORD_UNCOMPROMISED_COUNT > 0 ?
+                Password::min(User::PASSWORD_MIN_LENGHT)
+                    ->uncompromised(User::PASSWORD_UNCOMPROMISED_COUNT) :
+                Password::min(User::PASSWORD_MIN_LENGHT);
+        });
     }
 }
