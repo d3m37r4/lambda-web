a61b9365a35cd81774ed1abdae9e0b8d56a8195b
d3m37r4 <dmitrij.isakoff2012@yandex.ru>
2022-05-10 20:11:29 +0700

    Request validator for control panel 'store' method of 'UsersManagementController' controller inherits rules from the 'StoreUserRequest' class.

--- app/Http/Requests/Admin/AdminStoreUserRequest.php
+++ app/Http/Requests/Admin/AdminStoreUserRequest.php
@@ -7,27 +7,14 @@ use App\Http\Requests\StoreUserRequest;
 class AdminStoreUserRequest extends StoreUserRequest
 {
     /**
-     * Determine if the user is authorized to make this request.
-     *
-     * @return bool
-     */
-    public function authorize(): bool
-    {
-        return true;
-    }
-
-    /**
      * Get the validation rules that apply to the request.
      *
      * @return array
      */
     public function rules(): array
     {
-        return [
-//            'name' => ['required', 'string', 'max:255', 'unique:users'],
-//            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
-//            'password' => ['required', 'string', 'min:6', 'confirmed'],
-//            'role' => ['required', 'string'],
-        ];
+        return array_merge(parent::rules(), [
+            'role' => ['required', 'string']
+        ]);
     }
 }
