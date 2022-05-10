7e3f7c8facbe4227956fc908ee762cee507c64a1
d3m37r4 <dmitrij.isakoff2012@yandex.ru>
2022-05-10 21:20:45 +0700

    Added a class of policies for actions with the 'Role' model.

---
+++ app/Policies/RolePolicy.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\User;
+use App\Models\Role;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class RolePolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Determine whether the user can view any models.
+     *
+     * @param User $user
+     * @return bool
+     */
+    public function viewAny(User $user): bool
+    {
+        return $user->hasPermissionTo('manage_roles') || $user->hasRole(Role::ROLE_OWNER);
+    }
+
+    /**
+     * Determine whether the user can view the model.
+     *
+     * @param User $user
+     * @param Role $role
+     * @return bool
+     */
+    public function view(User $user, Role $role): bool
+    {
+        return true;
+    }
+
+    /**
+     * Determine whether the user can create models.
+     *
+     * @param User $user
+     * @return bool
+     */
+    public function create(User $user): bool
+    {
+        return $user->hasPermissionTo('manage_roles') || $user->hasRole(Role::ROLE_OWNER);
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     *
+     * @param User $user
+     * @param Role $role
+     * @return bool
+     */
+    public function update(User $user, Role $role): bool
+    {
+        /**
+         * The "Owner" role can only be changed by owner.
+         */
+        if ($role->isOwnerRole() && !$user->hasRole(Role::ROLE_OWNER)) {
+            return false;
+        }
+
+        return $user->hasPermissionTo('edit_roles') || $user->hasRole(Role::ROLE_OWNER);
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     *
+     * @param User $user
+     * @param Role $role
+     * @return bool
+     */
+    public function delete(User $user, Role $role): bool
+    {
+        /**
+         * The "Owner" role cannot be deleted.
+         */
+        if ($role->isOwnerRole()) {
+            return false;
+        }
+
+        return $user->hasPermissionTo('delete_roles') || $user->hasRole(Role::ROLE_OWNER);
+    }
+}
