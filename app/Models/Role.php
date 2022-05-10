837e26a4abd9dfe472439cb0df984ca69f8ff420
d3m37r4 <dmitrij.isakoff2012@yandex.ru>
2022-05-10 21:21:12 +0700

    Extended the 'Role' model class.

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -11,8 +11,33 @@ use Spatie\Permission\Models\Role as SpatieRole;
  */
 class Role extends SpatieRole
 {
+    /**
+     * Constants defining the names of standard roles in the system.
+     *
+     * @note These constants are used in the system. There may be problems when changing and deleting.
+     */
     const ROLE_OWNER = 'owner';
     const ROLE_ADMIN = 'admin';
     const ROLE_MODER = 'moder';
     const ROLE_USER = 'user';
+
+    /**
+     * Checks whether the specified role is an owner role.
+     *
+     * @return bool
+     */
+    public function isOwnerRole(): bool
+    {
+       return $this->name === Role::ROLE_OWNER;
+    }
+
+    /**
+     * Checks whether the specified role is not the owner's role.
+     *
+     * @return bool
+     */
+    public function isNotOwnerRole(): bool
+    {
+        return $this->name != Role::ROLE_OWNER;
+    }
 }
