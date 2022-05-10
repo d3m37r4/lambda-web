efd08bf4a0b8c40c9336872c1891ffd31f3c6244
d3m37r4 <dmitrij.isakoff2012@yandex.ru>
2022-05-10 21:18:41 +0700

    Added translations for the roles section.

--- resources/lang/en/roles.php
+++ resources/lang/en/roles.php
@@ -4,6 +4,10 @@ use App\Models\Role;
 
 return [
     'role' => 'Role',
+    'management' => 'Role management',
+    'edit' => 'Editing a role: :role',
+    'delete' => 'Delete role',
+    'confirm_delete' => "Do you really want to delete role ':role'?",
     'list' => [
         Role::ROLE_OWNER => 'Owner',
         Role::ROLE_ADMIN => 'Administrator',
--- resources/lang/ru/roles.php
+++ resources/lang/ru/roles.php
@@ -4,6 +4,10 @@ use App\Models\Role;
 
 return [
     'role' => 'Роль',
+    'management' => 'Управление ролями',
+    'edit' => 'Редактирование роли: :role',
+    'delete' => 'Удалить роль',
+    'confirm_delete' => "Вы действительно хотите удалить роль ':role'?",
     'list' => [
         Role::ROLE_OWNER => 'Владелец',
         Role::ROLE_ADMIN => 'Администратор',
