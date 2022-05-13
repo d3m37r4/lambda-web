fc18d4fdcf8429220d285b064738b30780791d15
d3m37r4 <dmitrij.isakoff2012@yandex.ru>
2022-05-13 14:17:03 +0700

    Minor changes in 'Map' model class. Added a constant that determines size of server map name. Added size of 'name' field for migration associated with 'Map' model class.

--- app/Models/Map.php
+++ app/Models/Map.php
@@ -12,16 +12,30 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
 class Map extends Model
 {
     /**
+     * The maximum buffer size required to store a map's name.
+     * See: https://github.com/alliedmodders/amxmodx/blob/49a8b959b3a73f244327e6df6f8aedac54927058/plugins/include/amxconst.inc#L35
+     *
+     * @var int
+     */
+    const MAX_MAPNAME_LENGTH = 64;
+
+    /**
+     * The attributes that are mass assignable.
+     *
      * @var array
      */
     protected $fillable = ['name'];
 
     /**
+     * The attributes that should be mutated to dates.
+     *
      * @var array
      */
     protected $dates = ['created_at', 'updated_at'];
 
     /**
+     * The attributes that should be hidden for arrays.
+     *
      * @var array
      */
     protected $hidden = ['created_at', 'updated_at'];
