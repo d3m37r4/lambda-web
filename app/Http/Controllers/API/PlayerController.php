86c6c8f6b0c0750f8d8a92fd85f56c9ba73ae4fb
d3m37r4 <dmitrij.isakoff2012@yandex.ru>
2022-05-28 12:10:12 +0700

    Changes in the methods of the 'PlayerController' controller.

--- app/Http/Controllers/API/PlayerController.php
+++ app/Http/Controllers/API/PlayerController.php
@@ -2,9 +2,10 @@
 
 namespace App\Http\Controllers\API;
 
-use App\Models\PlayerSession;
-use App\Models\Player;
 use App\Models\Server;
+use App\Models\Player;
+use App\Models\PlayerSession;
+use App\Services\PlayerAction;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Http\JsonResponse;
@@ -16,43 +17,23 @@ class PlayerController extends Controller
      * Get information about connected player.
      *
      * @param Request $request
+     * @param Server $server
      * @return JsonResponse
      */
-    public function connect(Request $request): JsonResponse
+    public function connect(Request $request, Server $server): JsonResponse
     {
         // TODO: Add request validator
-        $server = Server::find($request->attributes->get('server_id'));
-        $player = Player::firstOrNew(
-            [
-                'server_id' => $server->id,
-                'authid' => $request->input('authid'),
-                'auth_type' => $request->input('auth_type')
-            ],
-            [
-                'server_id' => $server->id,
-                'authid' => $request->input('authid'),
-                'ip' => $request->input('ip'),
-                'auth_type' => $request->input('auth_type')
-            ]
-        );
-
-        $player->name = $request->input('name');
-
-        if ($player->exists) {
-            $action = 'loaded';
-            $player->update();
-        } else {
-            $action = 'created';
-            $player->save();
-        }
+        $player = PlayerAction::findOrCreate($server, $request);
+        $action = PlayerAction::getAction($player);
+        PlayerAction::setCurrentName($player, $request);
 
         if ($request->has('session_id')) {
             $session = PlayerSession::find($request->input('session_id'));
         } else {
-            $session = $player->active_session();
+            $session = $player->active_session;
         }
 
-        if (!$session) {
+        if (empty($session)) {
             $session = (new PlayerSession)->fill([
                 'player_id' => $player->id,
                 'server_id' => $server->id,
@@ -88,12 +69,12 @@ class PlayerController extends Controller
      * Updates information about disconnected player.
      *
      * @param Request $request
+     * @param Server $server
+     * @param Player $player
      * @return JsonResponse
      */
-    public function disconnect(Request $request): JsonResponse
+    public function disconnect(Request $request, Server $server, Player $player): JsonResponse
     {
-        $server = Server::find($request->attributes->get('server_id'));
-        $player = Player::find($request->input('player_id'));
         $session = PlayerSession::find($request->input('session_id'));
 
         if ($player && $player->exists) {
