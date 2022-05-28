52111607438b1171618dea9a9e89467c0da865e9
d3m37r4 <dmitrij.isakoff2012@yandex.ru>
2022-05-28 12:05:09 +0700

    Updated routes for game server API.

--- routes/game-server-api.php
+++ routes/game-server-api.php
@@ -8,13 +8,14 @@ use Illuminate\Support\Facades\Route;
 Route::group(['prefix' => 'servers'], function () {
     Route::post('auth', GameServerAuthController::class);
     Route::group([
+        'prefix' => '{server}',
         'middleware' => ['game-server-api', 'access_token']
     ], function () {
-        Route::post('{server}/info', [GameServerActionsController::class, 'info']);
-        Route::post('{server}/ping', [GameServerActionsController::class, 'ping']);
+        Route::post('info', [GameServerActionsController::class, 'info']);
+        Route::post('ping', [GameServerActionsController::class, 'ping']);
         Route::group(['prefix' => 'players'], function () {
             Route::post('connect', [PlayerController::class, 'connect']);
-            Route::post('disconnect', [PlayerController::class, 'disconnect']);
+            Route::post('{player}/disconnect', [PlayerController::class, 'disconnect']);
         });
     });
 });
