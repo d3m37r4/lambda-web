03b6a229836b5d241ecf8badae2236d70a09779e
d3m37r4 <dmitrij.isakoff2012@yandex.ru>
2022-06-26 12:17:59 +0700

    Added the 'PlayerSessionAction' service. Renamed the player action controller.

---
+++ app/Http/Controllers/API/GameServerPlayersActionsController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Models\Server;
+use App\Models\Player;
+use App\Services\PlayerAction;
+use App\Services\PlayerSessionAction;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Response;
+
+class GameServerPlayersActionsController extends Controller
+{
+    /**
+     * Get information about connected player.
+     *
+     * @param Request $request
+     * @param Server $server
+     * @return JsonResponse
+     */
+    public function connect(Request $request, Server $server): JsonResponse
+    {
+        // TODO: Add request validator
+        $player = PlayerAction::findOrCreate($server, $request);
+        PlayerAction::setCurrentName($player, $request);
+
+        $action = PlayerAction::getAction($player);
+        $session = PlayerSessionAction::getActiveSession($server, $player);
+        $server->update();  // To update online players
+
+        return Response::json([
+            'player_id' => $player->id,
+            'session_id' => $session->id,
+            'action' => $action
+        ]);
+    }
+
+    /**
+     * Updates information about disconnected player.
+     *
+     * @param Request $request
+     * @param Server $server
+     * @param Player $player
+     * @return JsonResponse
+     */
+    public function disconnect(Request $request, Server $server, Player $player): JsonResponse
+    {
+        // TODO: Add request validator
+        PlayerAction::setCurrentName($player, $request);
+
+        $player->active_session->close();
+        $server->update();  // To update online players
+
+        return Response::json();
+    }
+}
