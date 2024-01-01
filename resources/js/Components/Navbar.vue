5523418629ac8258f1353977d0fac7d2931e4b26
d3m37r4 <dmitrij.isakoff2012@yandex.ru>
2024-01-01 12:24:28 +0700

    Added a link to edit a user profile in the drop-down menu.

--- resources/js/Components/Navbar.vue
+++ resources/js/Components/Navbar.vue
@@ -45,14 +45,14 @@
                                     </svg>
                                     <span>Панель управления</span>
                                 </Link>
-                                <a href="/" class="flex items-center space-x-3 text-gray-500 focus:outline-none">
+                                <Link :href="route('profile.edit')" class="flex items-center space-x-3 text-gray-500 focus:outline-none">
                                     <svg class="w-5 h-5" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none">
                                         <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
                                         <path d="M10.325 4.317c.426 -1.756 2.924 -1.756 3.35 0a1.724 1.724 0 0 0 2.573 1.066c1.543 -.94 3.31 .826 2.37 2.37a1.724 1.724 0 0 0 1.065 2.572c1.756 .426 1.756 2.924 0 3.35a1.724 1.724 0 0 0 -1.066 2.573c.94 1.543 -.826 3.31 -2.37 2.37a1.724 1.724 0 0 0 -2.572 1.065c-.426 1.756 -2.924 1.756 -3.35 0a1.724 1.724 0 0 0 -2.573 -1.066c-1.543 .94 -3.31 -.826 -2.37 -2.37a1.724 1.724 0 0 0 -1.065 -2.572c-1.756 -.426 -1.756 -2.924 0 -3.35a1.724 1.724 0 0 0 1.066 -2.573c-.94 -1.543 .826 -3.31 2.37 -2.37c1 .608 2.296 .07 2.572 -1.065z"></path>
                                         <path d="M9 12a3 3 0 1 0 6 0a3 3 0 0 0 -6 0"></path>
                                     </svg>
                                     <span>Профиль</span>
-                                </a>
+                                </Link>
                                 <a href="/" class="flex items-center space-x-3 text-gray-500 focus:outline-none">
                                     <svg class="w-5 h-5" viewBox="0 0 35 35" fill="currentColor">
                                         <path d="M13.2,18.456H8.475a1.25,1.25,0,1,1,0-2.5H13.2a1.25,1.25,0,0,1,0,2.5Z"/>
