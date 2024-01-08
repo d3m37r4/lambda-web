acaf8bd13ca7bfbc0e1d402930cd9fcd08dd7864
d3m37r4 <dmitrij.isakoff2012@yandex.ru>
2024-01-08 14:04:20 +0700

    Update composer.lock and tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,7 +19,7 @@ module.exports = {
         },
     },
     plugins: [
-        require('@tailwindcss/forms'),
+        // require('@tailwindcss/forms'),
         require("daisyui"),
     ],
     // daisyUI config (optional - here are the default values)
