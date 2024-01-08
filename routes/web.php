788483b3165eeae540d59293cda12f9f233ae805
d3m37r4 <dmitrij.isakoff2012@yandex.ru>
2024-01-08 13:59:11 +0700

    Removed unused route.

--- routes/web.php
+++ routes/web.php
@@ -20,10 +20,6 @@ Route::get('/', function () {
     return Inertia::render('Index');
 });
 
-Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
 Route::group([
     'middleware' => ['web', 'auth']
 ], function () {
