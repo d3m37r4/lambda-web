06334cd06f67c7f6942151f493fb703da1d831f7
d3m37r4 <dmitrij.isakoff2012@yandex.ru>
2024-05-15 14:48:43 +0700

    Removed unnecessary div block tags.

--- resources/js/Components/Dashboard/SearchForm.vue
+++ resources/js/Components/Dashboard/SearchForm.vue
@@ -1,20 +1,14 @@
 <template>
     <div class="join">
-        <div>
-            <div>
-                <input class="input join-item input-bordered" placeholder="Введите текст..."/>
-            </div>
-        </div>
+        <input class="input join-item input-bordered" placeholder="Введите текст..."/>
         <select class="select join-item select-bordered">
             <option disabled selected>Фильтр</option>
             <option>Логин</option>
             <option>E-mail</option>
             <option>Роль</option>
         </select>
-        <div>
-            <button class="btn join-item btn-neutral">
-                <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 24 24"><path d="M16.32 14.9l5.39 5.4a1 1 0 0 1-1.42 1.4l-5.38-5.38a8 8 0 1 1 1.41-1.41zM10 16a6 6 0 1 0 0-12 6 6 0 0 0 0 12z" /></svg>
-            </button>
-        </div>
+        <button class="btn join-item btn-neutral">
+            <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 24 24"><path d="M16.32 14.9l5.39 5.4a1 1 0 0 1-1.42 1.4l-5.38-5.38a8 8 0 1 1 1.41-1.41zM10 16a6 6 0 1 0 0-12 6 6 0 0 0 0 12z" /></svg>
+        </button>
     </div>
 </template>
