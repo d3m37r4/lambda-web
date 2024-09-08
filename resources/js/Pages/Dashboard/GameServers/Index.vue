439242c19278737efd716ef7cb790ef1df173916
d3m37r4 <dmitrij.isakoff2012@yandex.ru>
2024-09-08 15:22:29 +0700

    Added a link to go to more information about the server.

--- resources/js/Pages/Dashboard/GameServers/Index.vue
+++ resources/js/Pages/Dashboard/GameServers/Index.vue
@@ -137,6 +137,11 @@ const confirmDeleteGameServer = (id) => {
                             <td>{{ game_server.created_at }}</td>
                             <td>{{ game_server.updated_at }}</td>
                             <td>
+                                <div class="lg:tooltip" data-tip="О сервере">
+                                    <Link :href="route('dashboard.game-servers.show', game_server.id)" class="btn btn-sm btn-circle btn-outline btn-info me-1">
+                                        <svg class="h-5 w-5" viewBox="0 0 24 24" fill="currentColor"><path d="M12 9a3.02 3.02 0 0 0-3 3c0 1.642 1.358 3 3 3 1.641 0 3-1.358 3-3 0-1.641-1.359-3-3-3z"/><path d="M12 5c-7.633 0-9.927 6.617-9.948 6.684L1.946 12l.105.316C2.073 12.383 4.367 19 12 19s9.927-6.617 9.948-6.684l.106-.316-.105-.316C21.927 11.617 19.633 5 12 5zm0 12c-5.351 0-7.424-3.846-7.926-5C4.578 10.842 6.652 7 12 7c5.351 0 7.424 3.846 7.926 5-.504 1.158-2.578 5-7.926 5z"/></svg>
+                                    </Link>
+                                </div>
                                 <div class="lg:tooltip" data-tip="Редактировать">
                                     <Link :href="route('dashboard.game-servers.edit', game_server.id)" class="btn btn-sm btn-circle btn-outline btn-accent me-1">
                                         <svg class="h-5 w-5" viewBox="0 0 24 24" fill="currentColor"><g id="info"/><g id="icons"><g id="edit"><path d="M2,20c0,1.1,0.9,2,2,2h2.6L2,17.4V20z"/><path d="M21.6,5.6l-3.2-3.2c-0.8-0.8-2-0.8-2.8,0l-0.2,0.2C15,3,15,3.6,15.4,4L20,8.6c0.4,0.4,1,0.4,1.4,0l0.2-0.2    C22.4,7.6,22.4,6.4,21.6,5.6z"/><path d="M14,5.4c-0.4-0.4-1-0.4-1.4,0l-9.1,9.1C3,15,3,15.6,3.4,16L8,20.6c0.4,0.4,1,0.4,1.4,0l9.1-9.1c0.4-0.4,0.4-1,0-1.4    L14,5.4z"/></g></g></svg>
