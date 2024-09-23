764e05fb07cc18152580277685e4552ebd6bc60c
d3m37r4 <dmitrij.isakoff2012@yandex.ru>
2024-09-23 20:46:33 +0700

    Updated component 'GameServer/Edit.vue' to use `useAuthToken` composable for token generation.

--- resources/js/Pages/Dashboard/GameServers/Edit.vue
+++ resources/js/Pages/Dashboard/GameServers/Edit.vue
@@ -1,12 +1,11 @@
 <script setup>
 import DashboardLayout from '@/Layouts/Dashboard.vue';
-import { useForm, router } from '@inertiajs/vue3';
-import { ref } from "vue";
+import { useForm } from '@inertiajs/vue3';
+import { useAuthToken } from '@/Composables/useAuthToken.js';
 import InputError from "@/Components/InputError.vue";
 import BackButton from "@/Components/BackButton.vue";
 import UpdateButton from "@/Components/UpdateButton.vue";
 import CopyToClipboard from "@/Components/CopyToClipboard.vue";
-import toast from "@/Store/toast";
 
 defineOptions({
     layout: DashboardLayout
@@ -25,28 +24,7 @@ const form = useForm({
     rcon: '',
 });
 
-const generatingToken = ref(false)
-const generateAuthToken = () => {
-    if (generatingToken.value) return;
-
-    generatingToken.value = true;
-    router.reload({
-        only: ['auth_token'],
-        preserveState: true,
-        preserveScroll: true,
-        onSuccess: (page) => {
-            form.auth_token = page.props.auth_token;
-            toast.add({
-                status: 'success',
-                message: 'Новый токен успешно сгенерирован.',
-            });
-            generatingToken.value = false;
-        },
-        onError: () => {
-            generatingToken.value = false;
-        },
-    });
-};
+const { generatingAuthToken, generateAuthToken } = useAuthToken(form);
 
 const update = () => {
     form.put(route('dashboard.game-servers.update', props.gameServer));
@@ -137,7 +115,7 @@ const update = () => {
                             </CopyToClipboard>
                         </div>
                         <div class="lg:tooltip" data-tip="Сгенерировать новый токен">
-                            <button type="button" @click="generateAuthToken" class="btn join-item btn-neutral" :disabled="generatingToken">
+                            <button type="button" @click="generateAuthToken" class="btn join-item btn-neutral" :disabled="generatingAuthToken">
                                 <svg class="h-5 w-5" fill="currentColor" viewBox="-1.5 -2.5 24 24" ><path d="m4.859 5.308 1.594-.488a1 1 0 0 1 .585 1.913l-3.825 1.17a1 1 0 0 1-1.249-.665L.794 3.413a1 1 0 1 1 1.913-.585l.44 1.441C5.555.56 10.332-1.035 14.573.703a9.381 9.381 0 0 1 5.38 5.831 1 1 0 1 1-1.905.608A7.381 7.381 0 0 0 4.86 5.308zm12.327 8.195-1.775.443a1 1 0 1 1-.484-1.94l3.643-.909a.997.997 0 0 1 .61-.08 1 1 0 0 1 .84.75l.968 3.88a1 1 0 0 1-1.94.484l-.33-1.322a9.381 9.381 0 0 1-16.384-1.796l-.26-.634a1 1 0 1 1 1.851-.758l.26.633a7.381 7.381 0 0 0 13.001 1.25z"/></svg>
                             </button>
                         </div>
