3533f769fd08f34739aa4e6784bf268a560b9d8b
d3m37r4 <dmitrij.isakoff2012@yandex.ru>
2024-09-29 14:40:39 +0700

    Cleanup.

--- config/app.php
+++ config/app.php
@@ -54,7 +54,7 @@ return [
 
     'url' => env('APP_URL', 'http://localhost'),
 
-    'asset_url' => env('ASSET_URL', null),
+    'asset_url' => env('ASSET_URL'),
 
     /*
     |--------------------------------------------------------------------------
@@ -228,8 +228,6 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-        'HelperAccessToken' => App\Helpers\HelperAccessToken::class,
-
     ],
 
 ];
