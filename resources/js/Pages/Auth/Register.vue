d9db600fdeaa9fbf2f705c47e323120735b0838d
d3m37r4 <dmitrij.isakoff2012@yandex.ru>
2024-11-25 13:36:13 +0700

    Refactoring the user registration form.

--- resources/js/Pages/Auth/Register.vue
+++ resources/js/Pages/Auth/Register.vue
@@ -1,15 +1,18 @@
 <script setup>
 import { Head, Link, useForm } from '@inertiajs/vue3';
+import InputError from "@/Components/InputError.vue";
 
 defineProps({
     title: String
 });
+
 const form = useForm({
     login: '',
     email: '',
     password: '',
     password_confirmation: '',
 });
+
 const submit = () => {
     form.post(route('register'), {
         onFinish: () => form.reset('password', 'password_confirmation'),
@@ -19,13 +22,15 @@ const submit = () => {
 
 <template>
     <Head :title="title" />
-    <div class="container w-full lg:max-w-lg">
-        <h1 class="mb-4 text-xl">{{ title }}</h1>
-        <div class="p-4 bg-base-200 rounded-box">
-            <form @submit.prevent="submit" class="space-y-4">
+    <div class="container w-full lg:max-w-lg space-y-4">
+        <div class="flex items-center justify-between mx-4">
+            <h1 class="text-xl">{{ title }}</h1>
+        </div>
+        <form @submit.prevent="submit">
+            <div class="bg-base-200 rounded-box p-4">
                 <div>
-                    <label class="label">
-                        <span class="text-base label-text">Login</span>
+                    <label for="login" class="label">
+                        <span class="text-base label-text">{{ ('Логин') }}</span>
                     </label>
                     <input
                         id="login"
@@ -36,13 +41,11 @@ const submit = () => {
                         autofocus
                         autocomplete="login"
                     />
-                    <div v-show="form.errors.login">
-                        <p class="text-sm text-red-600">{{ form.errors.login }}</p>
-                    </div>
+                    <InputError :message="form.errors.login" />
                 </div>
                 <div>
-                    <label class="label">
-                        <span class="text-base label-text">Email</span>
+                    <label for="email" class="label">
+                        <span class="text-base label-text">{{ ('Эл. почта') }}</span>
                     </label>
                     <input
                         id="email"
@@ -50,16 +53,13 @@ const submit = () => {
                         class="input input-bordered w-full focus:ring-1 focus:ring-offset-2 focus:ring-offset-base-200 focus:ring-orange-500"
                         v-model="form.email"
                         required
-                        autofocus
-                        autocomplete="username"
+                        autocomplete="email"
                     />
-                    <div v-show="form.errors.email">
-                        <p class="text-sm text-red-600">{{ form.errors.email }}</p>
-                    </div>
+                    <InputError :message="form.errors.email" />
                 </div>
                 <div>
-                    <label class="label">
-                        <span class="text-base label-text">Password</span>
+                    <label for="password" class="label">
+                        <span class="text-base label-text">{{ ('Пароль') }}</span>
                     </label>
                     <input
                         id="password"
@@ -67,16 +67,13 @@ const submit = () => {
                         class="input input-bordered w-full focus:ring-1 focus:ring-offset-2 focus:ring-offset-base-200 focus:ring-orange-500"
                         v-model="form.password"
                         required
-                        autofocus
-                        autocomplete="username"
+                        autocomplete="new-password"
                     />
-                    <div v-show="form.errors.password">
-                        <p class="text-sm text-red-600">{{ form.errors.password }}</p>
-                    </div>
+                    <InputError :message="form.errors.password" />
                 </div>
                 <div>
-                    <label class="label">
-                        <span class="text-base label-text">Confirm Password</span>
+                    <label for="password_confirmation" class="label">
+                        <span class="text-base label-text">{{ ('Подтверждение пароля') }}</span>
                     </label>
                     <input
                         id="password_confirmation"
@@ -84,20 +81,19 @@ const submit = () => {
                         class="input input-bordered w-full focus:ring-1 focus:ring-offset-2 focus:ring-offset-base-200 focus:ring-orange-500"
                         v-model="form.password_confirmation"
                         required
-                        autofocus
                         autocomplete="new-password"
                     />
-                    <div v-show="form.errors.password_confirmation">
-                        <p class="text-sm text-red-600">{{ form.errors.password_confirmation }}</p>
-                    </div>
+                    <InputError :message="form.errors.password_confirmation" />
                 </div>
-                <div class="flex items-center justify-between">
+                <div class="flex items-center justify-between my-4">
                     <Link :href="route('login')" class="text-sm text-gray-600 hover:underline hover:text-orange-500">
                         {{ ('У меня уже есть аккаунт') }}
                     </Link>
-                    <button class="btn btn-success normal-case" :class="{ 'loading loading-spinner': form.processing }">{{ ('Зарегистрироваться') }}</button>
                 </div>
-            </form>
-        </div>
+            </div>
+            <div class="flex justify-end m-4">
+                <button class="btn btn-success normal-case" :disabled="!form.isDirty">{{ ('Зарегистрироваться') }}</button>
+            </div>
+        </form>
     </div>
 </template>
