f3d90cc2a7d917d373e344fa3a8ff02731268090
d3m37r4 <dmitrij.isakoff2012@yandex.ru>
2024-11-25 14:27:49 +0700

    Feature test of the new user registration route has been refactored.

--- tests/Feature/Auth/RegistrationTest.php
+++ tests/Feature/Auth/RegistrationTest.php
@@ -2,30 +2,54 @@
 
 namespace Tests\Feature\Auth;
 
+use Database\Seeders\PermissionSeeder;
+use Database\Seeders\RoleSeeder;
+use Tests\TestCase;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Tests\TestCase;
+use Inertia\Testing\AssertableInertia as Assert;
 
 class RegistrationTest extends TestCase
 {
     use RefreshDatabase;
 
-    public function test_registration_screen_can_be_rendered(): void
+    public function test_registration_page_can_be_rendered(): void
     {
-        $response = $this->get('/register');
-
-        $response->assertStatus(200);
+        $this->get(route('register'))
+            ->assertOk()
+            ->assertInertia(fn (Assert $page) => $page
+                ->component('Auth/Register')
+                ->where('errors', [])
+            );
     }
 
-    public function test_new_users_can_register(): void
+    public function test_it_should_return_errors_when_required_fields_fail_validation(): void
     {
-        $response = $this->post('/register', [
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-            'password' => 'password',
-            'password_confirmation' => 'password',
+        $this->get(route('register'));
+        $this->followingRedirects()
+            ->post(route('register'))
+            ->assertOk()
+            ->assertInertia(fn (Assert $page) => $page
+                ->component('Auth/Register')
+                ->has('errors.login')
+                ->has('errors.email')
+                ->has('errors.password')
+            );
+    }
+
+    public function test_new_user_can_register(): void
+    {
+        $this->seed([
+            PermissionSeeder::class,
+            RoleSeeder::class,
         ]);
 
+        $response = $this->post(route('register'), [
+                'login' => 'Test User',
+                'email' => 'test@example.com',
+                'password' => 'password',
+                'password_confirmation' => 'password',
+        ]);
         $this->assertAuthenticated();
         $response->assertRedirect(RouteServiceProvider::HOME);
     }
