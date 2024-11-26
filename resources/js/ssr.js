e0ef53aee8306209b0456564cfa3ddb56bbbf485
d3m37r4 <dmitrij.isakoff2012@yandex.ru>
2024-11-26 15:25:19 +0700

    Update lambda-tests.yml, app.js, ssr.js

--- resources/js/ssr.js
+++ resources/js/ssr.js
@@ -1,7 +1,7 @@
 import { createSSRApp, h } from 'vue';
 import { renderToString } from '@vue/server-renderer';
 import { createInertiaApp } from '@inertiajs/vue3';
-import { ZiggyVue } from '/vendor/tightenco/ziggy/dist/vue.m';
+import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/vue.m';
 import createServer from '@inertiajs/vue3/server';
 import AppLayout from './Layouts/Main.vue';
 
