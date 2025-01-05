dedd471fdd3250964d336f2038ef2b9cc9530c06
d3m37r4 <dmitrij.isakoff2012@yandex.ru>
2025-01-05 20:58:35 +0700

    Added new vue-components.

---
+++ resources/js/Components/RoleBadge.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { computed } from 'vue';
+import { trans } from 'laravel-vue-i18n';
+
+const props = defineProps({
+    role: {
+        type: String,
+        required: true,
+    },
+});
+
+const roleName = computed(() => {
+    return trans(`role.name.${props.role.toLowerCase()}`, 'role.name.unknown');
+});
+</script>
+
+<template>
+    <div class="badge badge-secondary rounded">
+        {{ roleName }}
+    </div>
+</template>
