e7f92e06e85115be4d864e6ad6b8e6108d9b8693
d3m37r4 <dmitrij.isakoff2012@yandex.ru>
2025-01-08 16:22:08 +0700

    Localize pagination labels

--- resources/js/Components/Pagination.vue
+++ resources/js/Components/Pagination.vue
@@ -1,28 +1,41 @@
+<script setup>
+import { Link } from '@inertiajs/vue3';
+import { trans } from 'laravel-vue-i18n';
+
+const props = defineProps({
+    links: Array,
+    items: Object,
+});
+
+const getLabel = (label) => {
+    if (label.includes('Previous')) {
+        return trans('pagination.previous');
+    }
+
+    if (label.includes('Next')) {
+        return trans('pagination.next');
+    }
+
+    return label;
+};
+</script>
+
 <template>
     <div v-if="links.length > 3" class="flex items-center justify-between mx-4">
-        <!--        <div class="flex flex-1 justify-between sm:hidden">-->
-        <!--            <a href="#" class="relative inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50">Previous</a>-->
-        <!--            <a href="#" class="relative ml-3 inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50">Next</a>-->
-        <!--        </div>-->
-        <div>
-            <p class="text-sm">
-                {{ "Показано с" }} {{ items.from }} {{ 'по' }} {{ items.to }} {{ 'из' }} {{ items.total }} {{ 'записей.' }}
-            </p>
+        <div class="text-sm">
+            {{ $t('pagination.items_shown', { from: items.from, to: items.to, total: items.total }) }}
         </div>
         <div class="join">
-            <template v-for="(link, key) in links">
-                <div v-if="link.url === null" :key="key" class="join-item btn normal-case btn-disabled" v-html="link.label" />
-                <Link v-else :key="`link-${key}`" class="join-item btn normal-case" :class="{ 'btn-active text-orange-500': link.active }" :href="link.url" v-html="link.label" />
+            <template v-for="(link, index) in links" :key="`link-${index}`">
+                <div v-if="link.url === null"
+                     class="join-item btn normal-case btn-disabled"
+                     v-html="getLabel(link.label)" />
+                <Link v-else
+                      class="join-item btn normal-case"
+                      :class="{ 'btn-active text-orange-500': link.active }"
+                      :href="link.url"
+                      v-html="getLabel(link.label)" />
             </template>
         </div>
     </div>
 </template>
-
-<script setup>
-    import { Link } from '@inertiajs/vue3';
-
-    defineProps({
-        links: Array,
-        items: Object,
-    });
-</script>
